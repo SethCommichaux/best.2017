3dd4325f005bdf3da7429ecfb2ae3df56d9c7f5b
SethCommichaux <yofi01@gmail.com>
2017-10-07 11:25:45 -0400

    Create cluster_of_clusters.py

---
+++ cluster_of_clusters.py
@@ -0,0 +1,29 @@
+# A script to get first layer of hierarchically clustered samples in the IGC.
+# commandline arguments are only one of the following files: AGC.clsr, CGC.clstr, EGC.clstr, SPGC.clstr
+
+import sys
+
+f = sys.argv[1]
+o = open(f[:-6]+'cluster_of_clusters.txt','w')
+c = 0
+
+cluster_recruits = {} # dictionary where keys are cluster centroids and values are cluster members
+
+for i in open(f):
+	if i.startswith('>'):
+		continue
+	else:
+		c += 1
+		i = i.split('\t')[1].strip()
+		if i.endswith('*'):
+			x = i.strip(' *')
+			cluster_recruits[x] = []
+		else:
+			cluster_recruits[x].append(i.split(' at ')[0])
+
+for k,v in cluster_recruits.items():
+	o.write(k+'\t'+'\t'.join(v)+'\n') 
+
+o.close()
+
+print f+'\t'+str(c)+'\n' # prints total number of ORFs processed
